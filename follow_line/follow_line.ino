af829e48c60660a32df1c56f94f4003a3b1912d5
Rucha Dave <ruchadave16@gmail.com>
2022-10-06 15:31:45 -0400

    Create main file to run robot

---
+++ follow_line/follow_line.ino
@@ -0,0 +1,16 @@
+const int sensor1Pin = ;
+const int sensor2Pin = ;
+const int sensorMidPin = ;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
