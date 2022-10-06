3980d55e8b99dd7be4027f8c2c96b27daa5cba07
Rucha Dave <ruchadave16@gmail.com>
2022-10-06 15:56:36 -0400

    add code to move motor

--- follow_line/follow_line.ino
+++ follow_line/follow_line.ino
@@ -1,6 +1,12 @@
-const int sensor1Pin = A0;
-const int sensor2Pin = A1;
+const int sensorLeftPin = A0;
+const int sensorRightPin = A1;
 const int sensorMidPin = A2;
+
+const int boundaryValue = 600;
+
+int sensorLeftValue;
+int sensorRightValue;
+int sencorMidValue;
 
 #include <Adafruit_MotorShield.h>
 
@@ -16,11 +22,15 @@ void setup() {
     while (1)
   }
 
+  pinMode(sensorLeftPin, INPUT);
+  pinMode(sensorRightPin, INPUT);
+  pinMode(sensorMidPin, INPUT);
+
   leftMotor->setSpeed(150);  
   rightMotor->setSpeed(150);
 
   leftMotor->run(FORWARD);
-  rightMotor->run(BACKWARD);
+  rightMotor->run(FORWARD);
 
   leftMotor->run(RELEASE);
   rightMotor->run(RELEASE);
@@ -28,5 +38,31 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  sensorLeftValue = analogRead(sensorLeftPin);
+  sensorRightValue = analogRead(sensorRightPin);
+  sensorMidValue = analogRead(sensorMidValue);
+
+  if ((sensorMidValue < boundaryValue) && (sensorLeftValue < boundaryValue) && (sensorRightValue < boundaryValue)) {
+    leftMotor->run(FORWARD);
+    rightMotor->run(BACKWARD);
+
+    leftMotor->setSpeed(100);
+    rightMotor->setSpeed(100);
+  }
+
+  else if (sensorLeftValue > boundaryValue) {
+    rightMotor->setSpeed(200);
+  }
+
+  else if (sensorRightValue > boundaryValue) {
+    leftMotor->setSpeed(200);
+  }
   
+  else {
+    leftMotor->setSpeed(150);  
+    rightMotor->setSpeed(150);
+  }
+
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
 }
