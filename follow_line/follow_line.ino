f40ae7c8ae4b2eb6a8a2d716e51c532b1932fe84
ayushchakra <achakraborty@olin.edu>
2022-10-13 16:54:35 -0400

    debugged based on physical setup

--- follow_line/follow_line.ino
+++ follow_line/follow_line.ino
@@ -1,33 +1,33 @@
-const int sensorLeftPin = A0;
-const int sensorRightPin = A1;
+const int sensorLeftPin = A1;
+const int sensorRightPin = A0;
 const int sensorMidPin = A2;
 
-const int boundaryValue = 600;
+const int boundaryValue = 550;
 
 int sensorLeftValue;
 int sensorRightValue;
-int sencorMidValue;
+//int sensorMidValue;
 
 #include <Adafruit_MotorShield.h>
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  if (!AFMS.begin() {
+  if (!AFMS.begin()) {
     Serial.println("Check wiring");
-    while (1)
+    while (1);
   }
 
   pinMode(sensorLeftPin, INPUT);
   pinMode(sensorRightPin, INPUT);
-  pinMode(sensorMidPin, INPUT);
+//  pinMode(sensorMidPin, INPUT);
 
-  leftMotor->setSpeed(150);  
-  rightMotor->setSpeed(150);
+  leftMotor->setSpeed(30);  
+  rightMotor->setSpeed(30);
 
   leftMotor->run(FORWARD);
   rightMotor->run(FORWARD);
@@ -40,29 +40,34 @@ void loop() {
   // put your main code here, to run repeatedly:
   sensorLeftValue = analogRead(sensorLeftPin);
   sensorRightValue = analogRead(sensorRightPin);
-  sensorMidValue = analogRead(sensorMidValue);
+//  sensorMidValue = analogRead(sensorMidValue);
 
-  if ((sensorMidValue < boundaryValue) && (sensorLeftValue < boundaryValue) && (sensorRightValue < boundaryValue)) {
-    leftMotor->run(FORWARD);
-    rightMotor->run(BACKWARD);
+//  if ((sensorMidValue < boundaryValue) && (sensorLeftValue < boundaryValue) && (sensorRightValue < boundaryValue)) {
+//    leftMotor->run(FORWARD);
+//    rightMotor->run(BACKWARD);
+//
+//    leftMotor->setSpeed(20);
+//    rightMotor->setSpeed(20);
+//  }
 
-    leftMotor->setSpeed(100);
-    rightMotor->setSpeed(100);
-  }
-
-  else if (sensorLeftValue > boundaryValue) {
-    rightMotor->setSpeed(200);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  
+  if (sensorLeftValue > boundaryValue) {
+    Serial.println("Found Line");
+    leftMotor->setSpeed(0);
+    rightMotor->setSpeed(40);
   }
 
   else if (sensorRightValue > boundaryValue) {
-    leftMotor->setSpeed(200);
+    Serial.println("Found Other Line");
+    leftMotor->setSpeed(40);
+    rightMotor->setSpeed(0);
   }
   
   else {
-    leftMotor->setSpeed(150);  
-    rightMotor->setSpeed(150);
+    Serial.println("Go Straight");
+    leftMotor->setSpeed(10);  
+    rightMotor->setSpeed(10);
   }
-
-  leftMotor->run(RELEASE);
-  rightMotor->run(RELEASE);
 }
