2e77c5e6e2f243748297d494a43fb7bafbfc5d4f
ayushchakra <achakraborty@olin.edu>
2022-10-18 22:19:31 -0400

    added keyboard interrupt handling

--- follow_line/visualize_run.py
+++ follow_line/visualize_run.py
@@ -1,15 +1,45 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 import serial
 
 arduino_port = '/dev/ttyACM0'
 baud_rate = 9600
+# Data is expected to come in the format:
+# "{Variable_Name} {Variable_Value} {Current_time}"
 serial_port = serial.Serial(arduino_port, baud_rate, timeout=1)
 
-while True:
-    try:
+left_motor_values = []
+right_motor_values = []
+left_ir_values = []
+right_ir_values = []
+
+try:
+    while True:
         data = serial_port.readline().decode()
-    except UnicodeDecodeError:
-        continue
-        
+        print(data)
+except KeyboardInterrupt:
+    pass
+
+# import sys, signal
+# def signal_handler(signal, frame):
+#     print("\nprogram exiting gracefully")
+#     sys.exit(0)
+
+# signal.signal(signal.SIGINT, signal_handler)
+
+# try:
+#     while True:
+#         # data = serial_port.readline().decode()
+#         print(1)
+# except KeyboardInterrupt:
+#     print("here")
+#     pass
+
+# try:
+#     while True:
+#         pass # Do something
+# except KeyboardInterrupt:
+#     pass
+# # Do something. Program doesn't terminate after CTRL-C
+# print('hello world')
+    
